edfcc46d585c34d2b233d09158c2dd3868582769
Radik Asfandiyarov <rasfandiyarov@65apps.com>
2019-02-25 14:40:05 +0400

    test data

---
+++ app/src/main/java/com/a65apps/mapkitclustering/TestData.java
@@ -0,0 +1,38 @@
+package com.a65apps.mapkitclustering;
+
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import androidx.annotation.NonNull;
+
+public class TestData {
+    public static final int POINTS_COUNT = 100;
+    @NonNull
+    public static final Point POINT = new Point(56.863069, 53.219774);
+    private static final float MIN_LAT = 56.837725f;
+    private static final float MAX_LAT = 56.866513f;
+    private static final float MIN_LON = 53.205989f;
+    private static final float MAX_LON = 53.233493f;
+    @NonNull
+    public static List<Point> POINTS_LIST = new ArrayList<>(1000);
+    @NonNull
+    public static Point CLUSTER_POINT_0;
+    @NonNull
+    public static CameraPosition CAMERA_POSITION;
+
+    static {
+        for (int i = 0; i < POINTS_COUNT; i++) {
+            Random r = new Random();
+            float lat = MIN_LAT + r.nextFloat() * (MAX_LAT - MIN_LAT);
+            float lon = MIN_LON + r.nextFloat() * (MAX_LON - MIN_LON);
+            POINTS_LIST.add(new Point(lat, lon));
+        }
+
+        CLUSTER_POINT_0 = ClusterAnimator.calcCenter(POINTS_LIST);
+        CAMERA_POSITION = new CameraPosition(CLUSTER_POINT_0, 14.0f, 0.0f, 0.0f);
+    }
+}
