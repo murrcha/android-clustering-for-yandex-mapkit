b5676640c9e0e377ec90b7cc3ef32a827e0e21c6
Radik Asfandiyarov <rasfandiyarov@65apps.com>
2019-02-26 15:03:47 +0400

    apikey в secret.properties

--- app/src/main/java/com/a65apps/mapkitclustering/MainActivity.kt
+++ app/src/main/java/com/a65apps/mapkitclustering/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.a65apps.mapkitclustering
 
 import android.os.Bundle
-
 import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        MapKitFactory.setApiKey(BuildConfig.YANDEX_MAP_KIT_KEY)
+        MapKitFactory.initialize(this)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+    }
+
+    override fun onStop() {
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
