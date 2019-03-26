fc1fbf981de70b100b79a8a1d639c70a9fc2abbd
Radik Asfandiyarov <rasfandiyarov@65apps.com>
2019-03-26 14:15:05 +0400

    fix demo CustomPinProvider

--- demo/src/main/java/com/a65apps/mapkitclustering/CustomPinProvider.kt
+++ demo/src/main/java/com/a65apps/mapkitclustering/CustomPinProvider.kt
@@ -1,6 +1,5 @@
 package com.a65apps.mapkitclustering
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.PointF
 import com.a65apps.clustering.core.Cluster
@@ -12,30 +11,22 @@ import com.yandex.runtime.image.ImageProvider
 import com.yandex.runtime.ui_view.ViewProvider
 
 class CustomPinProvider(context: Context) : ClusterPinProvider {
-    private val clusterIconStyle =
-            IconStyle(PointF(0.5f, 0.5f), null, null,
-                    null, null, null, null)
-    private val pinIconStyle =
-            IconStyle(PointF(0.5f, 1f), null, null,
-                    null, null, null, null)
-    private val clusterResource =
-            YandexPinProvider.from(ImageProvider.fromResource(context, R.drawable.cluster),
-                    clusterIconStyle)
+    private val pinIconStyle = IconStyle(PointF(0.5f, 1f), null, null,
+            null, null, null, null)
     private val clusterView = ClusterPinView(context)
-    private val pinResource =
-            YandexPinProvider.from(ImageProvider.fromResource(context, R.drawable.pin),
-                    pinIconStyle)
-    @SuppressLint("PrivateResource")
-    private val xResource =
-            YandexPinProvider.from(
-                    ImageProvider.fromResource(context, R.drawable.abc_ic_star_black_16dp))
+    private val pinResource = ImageProvider.fromResource(context, R.drawable.pin)
+    private val pinProvider = YandexPinProvider.from(pinResource, pinIconStyle)
+    private val providers = mutableMapOf(Pair(1, pinProvider))
 
     override fun get(cluster: Cluster): YandexPinProvider {
-        return if (cluster.isCluster()) {
-            clusterView.setText(cluster.size().toString())
-            YandexPinProvider.from(ViewProvider(clusterView))
-        } else {
-            pinResource
-        }
+        val size = cluster.size()
+        return providers[size] ?: createClusterProvider(size)
+    }
+
+    private fun createClusterProvider(size: Int): YandexPinProvider {
+        clusterView.setText(size.toString())
+        val newProvider = YandexPinProvider.from(ViewProvider(clusterView))
+        providers[size] = newProvider
+        return newProvider
     }
 }
